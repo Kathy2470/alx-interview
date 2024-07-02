450b40db11541aae1ec1f90a18ebba2884d66150
kathy2470 <mukisakathy@gmail.com>
2024-07-02 23:06:38 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,15 @@
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
-    # Start with the first box (box 0)
-    visited = set()
-    stack = [0]
+    n = len(boxes)
+    unlocked = set([0])
+    queue = [0]
 
-    while stack:
-        box_index = stack.pop()
-        if box_index not in visited:
-            visited.add(box_index)
-            # Add all keys from the current box to the stack
-            for key in boxes[box_index]:
-                if key not in visited:
-                    stack.append(key)
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key not in unlocked and 0 <= key < n:
+                unlocked.add(key)
+                queue.append(key)
 
-    # Check if all boxes have been visited
-    return len(visited) == len(boxes)
+    return len(unlocked) == n
